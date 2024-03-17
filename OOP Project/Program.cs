7232b02587953c4254ff42d44a9be1055ad75596
sungju <sjim1218@gmail.com>
2024-03-17 20:35:47 +0900

    feasfas

--- OOP Project/Program.cs
+++ OOP Project/Program.cs
@@ -10,7 +10,7 @@ namespace OOP_Project
     {
         static void Main(string[] args)
         {
-            // Git Test ffdsfasfdsafdfdsaf
+            // Git Test ffdsfasfdsafdfdsaffasfsefawsfawe
             Customer customer = new Customer();
             List<MenuItem> menuItems = Initialize();
 
